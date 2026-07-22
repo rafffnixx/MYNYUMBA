9ffb9d6744f669f703fa2387a22c7313b5289015
Raphael Mwongela <rafffnixx@gmail.com>
2026-07-22 21:55:34 +0300

    Use environment variable for API URL in axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'https://raffcodes.tech/api';
+// Use environment variable for API URL
+const API_URL = import.meta.env.VITE_API_URL || 'https://raffcodes.tech/api';
 
 const api = axios.create({
   baseURL: API_URL,
